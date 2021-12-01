b6f0431d4818ce15b88578689bf07f5255d6c1f9
Mrudul Rajendra Vichare <90233845+vmrudul@users.noreply.github.com>
2021-12-01 19:49:54 +0530

    Add files via upload

---
+++ Anudip Practicals/AssignmentOperator.java
@@ -0,0 +1,18 @@
+public class AssignmentOperetor{
+public static void main(String args[]){
+int num1=100;
+int num2=20;
+num2=num1;
+System.out.println("=Output:"+num2);
+num2+=num1;
+System.out.println("=Output:"+num2);
+num2-=num1;
+System.out.println("=Output:"+num2);
+num2*=num1;
+System.out.println("=Output:"+num2);
+num2/=num1;
+System.out.println("=Output:"+num2);
+num2%=num1;
+System.out.println("=Output:"+num2);
+
+}}
