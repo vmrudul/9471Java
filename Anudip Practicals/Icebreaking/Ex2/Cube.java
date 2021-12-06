e3bf8154112f72304bf80c1d0821f745c938e816
Mrudul Rajendra Vichare <90233845+vmrudul@users.noreply.github.com>
2021-12-06 20:14:10 +0530

    Create Cube.java

---
+++ Anudip Practicals/Icebreaking/Ex2/Cube.java
@@ -0,0 +1,6 @@
+//Write a program in Java to display the cube of the number upto given an integer
+
+public class Cube{
+public static void main(String args[]){
+int i=4;
+System.out.println("Number is" +i+ "and the cube of" +i+ "is" +(i*i*i));}}
