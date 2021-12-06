308b0e27503cf31e49c64a688521ecb0487ef1bf
Mrudul Rajendra Vichare <90233845+vmrudul@users.noreply.github.com>
2021-12-06 22:04:39 +0530

    Add files via upload

---
+++ Anudip Practicals/Mrudul.java
@@ -0,0 +1,9 @@
+import java.util.scanner;
+class Mrudul{
+public static void main(String args[]){
+Scanner in=new Scanner(System.in);
+String name=in.next();
+System.out.print("Hello");
+System.out.println(name);
+}
+}
