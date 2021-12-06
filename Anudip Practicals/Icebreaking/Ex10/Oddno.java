69cd8d4d2458bebe5c7d31edf98e0b3258ee2dca
Mrudul Rajendra Vichare <90233845+vmrudul@users.noreply.github.com>
2021-12-06 21:40:15 +0530

    Create Oddno.java

---
+++ Anudip Practicals/Icebreaking/Ex10/Oddno.java
@@ -0,0 +1,10 @@
+//Write a Java program to print the odd numbers from 1 to 99. Prints one number per line.
+
+import java.util.*;
+public class Oddno{
+public static void main(String args[]){
+for(int a=1;a<=99;a++){
+if(a%2!=0){
+System.out.println(a);
+}
+}}}
