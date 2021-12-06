61d012169821e3c1de56908ef6229ce0b4d43bb3
Mrudul Rajendra Vichare <90233845+vmrudul@users.noreply.github.com>
2021-12-06 21:52:30 +0530

    Add files via upload

---
+++ Anudip Practicals/XYZ.java
@@ -0,0 +1,11 @@
+class Test{
+ String name="Mrudul";
+ void msg(){
+System.out.println("HELLO JAVA");}
+}
+public class XYZ{
+public static void main(String args[])
+{Test obj=new Test();
+System.out.println(obj.name);
+obj.msg();
+}}
