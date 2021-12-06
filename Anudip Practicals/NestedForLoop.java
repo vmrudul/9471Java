9fc13b9aa983b36998abac7c69c4517eddf69578
Mrudul Rajendra Vichare <90233845+vmrudul@users.noreply.github.com>
2021-12-06 21:45:42 +0530

    Add files via upload

---
+++ Anudip Practicals/NestedForLoop.java
@@ -0,0 +1,5 @@
+public class NestedForLoop{
+public static void main(String args[]){
+for(int i=1;i<=5;i++){
+for(int j=0;j<=3;j++)
+System.out.println(i+" "+j);}}}
---
+++ Anudip Practicals/hw.java
@@ -0,0 +1,4 @@
+public class hw{
+public static void main(String args[]){
+System.out.println("Hello World");
+}}
