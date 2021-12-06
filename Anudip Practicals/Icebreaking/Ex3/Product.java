8fdd61f2f063536c3aaf2d934715262fd7ce169a
Mrudul Rajendra Vichare <90233845+vmrudul@users.noreply.github.com>
2021-12-06 20:39:34 +0530

    Create Product.java

---
+++ Anudip Practicals/Icebreaking/Ex3/Product.java
@@ -0,0 +1,13 @@
+//Write a Java program that takes two numbers as input and display the product of two numbers.
+
+
+import java.util.*;
+public class Product{
+public static void main(String args[]){
+Scanner in=new Scanner(System.in);
+System.out.print("Enter 1st no.: ");
+int num1=in.nextInt();
+System.out.print("Enter 2nd no.: ");
+int num2=in.nextInt();
+System.out.println(num1*num2);
+}}
