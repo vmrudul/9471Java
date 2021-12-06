aeb10713d47c51963352f77ad9062dd276254383
Mrudul Rajendra Vichare <90233845+vmrudul@users.noreply.github.com>
2021-12-06 21:48:02 +0530

    Add files via upload

---
+++ Anudip Practicals/Break.java
@@ -0,0 +1,8 @@
+public class Break{
+public static void main(String args[]){
+for(int i=1;i<=10;i++){
+if(i==6){
+break;
+}
+System.out.println(i);}
+}}
---
+++ Anudip Practicals/Break3.java
@@ -0,0 +1,10 @@
+public class Break3{
+public static void main(String args[]){
+int i=1;
+while (i<=10){
+if(i==5){
+i++;
+break ;}
+System.out.println(i);
+i++;
+}}}
