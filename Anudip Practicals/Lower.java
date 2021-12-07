927b5d686cc14b5f1d8da4a3bf7a2279af407118
Mrudul Rajendra Vichare <90233845+vmrudul@users.noreply.github.com>
2021-12-07 21:40:26 +0530

    Create Lower.java

---
+++ Anudip Practicals/Lower.java
@@ -0,0 +1,17 @@
+//String to Lowercase
+
+import java.util.*;
+public class Lower {
+
+	public static void main(String[] args) {
+		Scanner in = new Scanner(System.in);
+		System.out.print("Input a String: ");
+		String line = in.nextLine();
+		line = line.toLowerCase();
+		System.out.println(line);
+		
+		
+
+	}
+
+}
