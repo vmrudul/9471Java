fc2908093c3bdfb8dad333055e8fb6b9fd7f7669
Mrudul Rajendra Vichare <90233845+vmrudul@users.noreply.github.com>
2022-01-13 20:10:46 +0530

    Update Ex3.java

--- Anudip Practicals/Collection/Ex3.java
+++ Anudip Practicals/Collection/Ex3.java
@@ -6,10 +6,10 @@ public class Ex3 {
 public static void main(String a[]) {
 	ArrayList<Integer> ar = new ArrayList<>();
 	ar.add(1); ar.add(2); ar.add(3); ar.add(4); ar.add(5); ar.add(6); ar.add(7);
-	/Iterator itr = ar.iterator();
-	/while(itr.hasNext()) {
-		/System.out.print(itr.next());
-	/}
+	/*Iterator itr = ar.iterator();
+	while(itr.hasNext()) {
+		System.out.print(itr.next());
+	}*/
 	for(Integer x : ar){
 		System.out.print(x + " ");
 	}
