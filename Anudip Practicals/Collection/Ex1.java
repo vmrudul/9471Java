d9267d84bf7b40a0e3d161cd12290b466abac5a8
Mrudul Rajendra Vichare <90233845+vmrudul@users.noreply.github.com>
2022-01-13 20:05:36 +0530

    Create Ex1.java

---
+++ Anudip Practicals/Collection/Ex1.java
@@ -0,0 +1,14 @@
+import java.io.*;
+import java.util.*;
+
+public class pr1 {
+public static void main(String a[]) {
+	String str1 = "Welcome";
+	char[] arr = {'f', 'o', 'r'};
+	String str2 = new String(arr);
+	String str3 = new String("today's lecture ");
+	System.out.println(str1);
+	System.out.println(str2);
+	System.out.println(str3);
+}
+}
